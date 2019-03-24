f54c94335911a80712b4a8ae9978036d92d0aef3
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-03-24 11:46:39 +0100

    sw05

--- sw05/app/src/main/java/ch/hslu/mobpro/sw05/MusicPlayerService.java
+++ sw05/app/src/main/java/ch/hslu/mobpro/sw05/MusicPlayerService.java
@@ -1,14 +1,36 @@
 package ch.hslu.mobpro.sw05;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
 public final class MusicPlayerService extends Service{
 
+    private Thread playThread = new Thread();
+    private static final int NOTIFICATION_ID = 123;
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        startPlayer();
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startPlayer() {
+        if (playThread != null && playThread.isAlive()){
+            return ;
+        }
+        startPlayThread();
+        startForeground(NOTIFICATION_ID, createNotification("Playing.."));
+    }
+
+    private Notification createNotification(String s) {
+
+        return null;
+    }
+
+    private void startPlayThread() {
+        playThread.start();
     }
 
     @Override
